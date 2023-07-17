31629e401eb288f9f5450b968995833616050829
amalgamallama <svetlana.svinina@gmail.com>
2023-07-18 00:56:21 +0300

    Вносит исправления (см комментарии)
    Корректирует марджины и паддинги для больших структурных блоков, модального окна. Добавляет возможность закрытия окна по клику на бэкдроп. Добавляет комментарии для js.

--- modal.js
+++ modal.js
@@ -1,15 +1,25 @@
-let openButtons = document.querySelectorAll('.open-modal-search, .search-button');
-let closeButton = document.querySelector('.modal-close-button');
+// Находим модальное окно
 let dialog = document.querySelector('.modal-search');
+// Находим все кнопки на странице, по которым должно открываться модальное окно
+let openButtons = document.querySelectorAll('.open-modal-search, .search-button');
+// Находим кнопку закрытия модального окна
+let closeButton = document.querySelector('.modal-close-button');
 
-console.log(openButtons);
+// Открытие модального окна по клику на кнопку
 for (let openButton of openButtons) {
   openButton.onclick = function () {
     dialog.showModal();
   };
 }
 
-
+// Закрытие модального окна по клику на кнопку
 closeButton.onclick = function () {
   dialog.close();
 };
+
+// Закрытие модального окна по клику на backdrop
+dialog.addEventListener('click', event => {
+  if (event.target === event.currentTarget) {
+      event.currentTarget.close()
+  }
+})
